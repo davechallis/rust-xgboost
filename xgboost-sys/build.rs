c652762312e301689182e8bc3995caea04b042ee
Dave Challis <dave@suicas.net>
2018-05-18 17:09:37 +0100

    Added quick check to avoid rebuilding xgboost source.

--- xgboost-sys/build.rs
+++ xgboost-sys/build.rs
@@ -2,16 +2,19 @@ extern crate bindgen;
 
 use std::process::Command;
 use std::env;
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 
 fn main() {
     // TODO: allow for dynamic/static linking
     // TODO: check whether rabit should be built/linked
-    // TODO: check whether build already completed
-    Command::new("./build.sh")
-        .current_dir("./xgboost")
-        .status()
-        .expect("Failed to execute XGBoost build.sh script.");
+    if (!Path::new("xgboost/lib").exists()) {
+        // TODO: better checks for build completion, currently xgboost's build script can run
+        // `make clean_all` if openmp build fails
+        Command::new("./build.sh")
+            .current_dir("./xgboost")
+            .status()
+            .expect("Failed to execute XGBoost build.sh script.");
+    }
 
      let bindings = bindgen::Builder::default()
         .header("wrapper.h")
