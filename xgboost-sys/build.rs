f132b8f5e0565440a69f1a6196a21fad699b37bd
Dave Challis <dave@suicas.net>
2018-06-01 10:50:09 +0100

    Use canonical paths. Added static linking for DMLC component.

--- xgboost-sys/build.rs
+++ xgboost-sys/build.rs
@@ -2,24 +2,27 @@ extern crate bindgen;
 
 use std::process::Command;
 use std::env;
-use std::path::{PathBuf, Path};
+use std::path::PathBuf;
+use std::fs::canonicalize;
 
 fn main() {
+    let xgb_root = canonicalize("xgboost").unwrap();
+
     // TODO: allow for dynamic/static linking
     // TODO: check whether rabit should be built/linked
-    if (!Path::new("xgboost/lib").exists()) {
+    if !xgb_root.join("lib").exists() {
         // TODO: better checks for build completion, currently xgboost's build script can run
         // `make clean_all` if openmp build fails
-        Command::new("./build.sh")
-            .current_dir("./xgboost")
+        Command::new(xgb_root.join("build.sh"))
+            .current_dir(&xgb_root)
             .status()
             .expect("Failed to execute XGBoost build.sh script.");
     }
 
      let bindings = bindgen::Builder::default()
         .header("wrapper.h")
-        .clang_arg("-Ixgboost/include")
-        .clang_arg("-Ixgboost/rabit/include")
+        .clang_arg(format!("-I{}", xgb_root.join("include").display()))
+        .clang_arg(format!("-I{}", xgb_root.join("rabit/include").display()))
         .generate()
         .expect("Unable to generate bindings.");
 
@@ -29,8 +32,10 @@ fn main() {
         .expect("Couldn't write bindings.");
 
     println!("cargo:rustc-flags=-l dylib=c++");
-    println!("cargo:rustc-link-search=xgboost/lib");
-    println!("cargo:rustc-link-search=xgboost/rabit/lib");
+    println!("cargo:rustc-link-search={}", xgb_root.join("lib").display());
+    println!("cargo:rustc-link-search={}", xgb_root.join("rabit/lib").display());
+    println!("cargo:rustc-link-search={}", xgb_root.join("dmlc-core").display());
     println!("cargo:rustc-link-lib=static=rabit_empty");
+    println!("cargo:rustc-link-lib=static=dmlc");
     println!("cargo:rustc-link-lib=static=xgboost");
 }
