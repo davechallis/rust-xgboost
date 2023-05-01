01aef8878ceb225cd04af0330c01a226aa2228ce
Lev Kokotov <hi@levthe.dev>
2023-05-01 10:11:22 -0700

    Manually include c++11 paths

--- xgboost-sys/build.rs
+++ xgboost-sys/build.rs
@@ -41,7 +41,10 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
+        .derive_default(true)
         .clang_args(&["-x", "c++", "-std=c++11"])
+        .clang_arg(format!("-I/usr/include/c++/11"))
+        .clang_arg(format!("-I/usr/include/x86_64-linux-gnu/c++/11"))
         .clang_arg(format!("-I{}", xgb_root.join("include").display()))
         .clang_arg(format!("-I{}", xgb_root.join("rabit/include").display()))
         .clang_arg(format!("-I{}", xgb_root.join("dmlc-core/include").display()));
